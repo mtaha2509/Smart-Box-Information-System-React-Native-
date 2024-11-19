e208ca5afe37754b21d43d38703d2a369301553b
masudsajid <masudsajid38@gmail.com>
2024-11-19 23:58:53 +0500

    initial log-in page

---
+++ screens/loginpage.js
@@ -0,0 +1,166 @@
+import { View, Text, Image, Pressable, TextInput, TouchableOpacity, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import Checkbox from "expo-checkbox"
+import Button from '../components/Button';
+import COLORS from '../utils/signlogincolors'
+
+const Login = ({ navigation }) => {
+    const [isPasswordShown, setIsPasswordShown] = useState(true);
+    const [isChecked, setIsChecked] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if (email === 'masudsajid38@gmail.com' && password === 'masud111') {
+            console.log("Go to main page")
+        } else {
+            Alert.alert("Error", "Invalid email or password.");
+        }
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+            <View style={{ flex: 1, marginHorizontal: 22 }}>
+                <View style={{ marginVertical: 22 }}>
+                    <Text style={{
+                        fontSize: 22,
+                        fontWeight: 'bold',
+                        marginVertical: 12,
+                        color: COLORS.black
+                    }}>
+                        Hi Welcome Back ! 👋
+                    </Text>
+
+                    <Text style={{
+                        fontSize: 16,
+                        color: COLORS.black
+                    }}>Let's get the parcel delivered</Text>
+                </View>
+
+                <View style={{ marginBottom: 12 }}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        marginVertical: 8
+                    }}>Email address</Text>
+
+                    <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: COLORS.black,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 22
+                    }}>
+                        <TextInput
+                            placeholder='Enter your email address'
+                            placeholderTextColor={COLORS.black}
+                            keyboardType='email-address'
+                            style={{
+                                width: "100%"
+                            }}
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                    </View>
+                </View>
+
+                <View style={{ marginBottom: 12 }}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        marginVertical: 8
+                    }}>Password</Text>
+
+                    <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: COLORS.black,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 22
+                    }}>
+                        <TextInput
+                            placeholder='Enter your password'
+                            placeholderTextColor={COLORS.black}
+                            secureTextEntry={isPasswordShown}
+                            style={{
+                                width: "100%"
+                            }}
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+
+                        <TouchableOpacity
+                            onPress={() => setIsPasswordShown(!isPasswordShown)}
+                            style={{
+                                position: "absolute",
+                                right: 12
+                            }}
+                        >
+                            {
+                                isPasswordShown == true ? (
+                                    <Ionicons name="eye-off" size={24} color={COLORS.black} />
+                                ) : (
+                                    <Ionicons name="eye" size={24} color={COLORS.black} />
+                                )
+                            }
+
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={{
+                    flexDirection: 'row',
+                    marginVertical: 6
+                }}>
+                    <Checkbox
+                        style={{ marginRight: 8 }}
+                        value={isChecked}
+                        onValueChange={setIsChecked}
+                        color={isChecked ? COLORS.primary : undefined}
+                    />
+
+                    <Text>Remember Me</Text> //handle later
+                </View>
+
+                <Button
+                    title="Login"
+                    filled
+                    style={{
+                        marginTop: 18,
+                        marginBottom: 4,
+                    }}
+                    onPress={handleLogin}
+                />
+
+
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    marginVertical: 22
+                }}>
+                    <Text style={{ fontSize: 16, color: COLORS.black }}>Don't have an account ? </Text>
+                    <Pressable
+                        onPress={()=> console.log("Will handle later")}
+                    >
+                        <Text style={{
+                            fontSize: 16,
+                            color: '#084abd',
+                            fontWeight: "bold",
+                            marginLeft: 6
+                        }}>Register</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Login
